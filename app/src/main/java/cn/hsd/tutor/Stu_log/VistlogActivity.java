054cdd6dda00b6dab14a57b80441e91b6d207c6e
wenqingjie <316400644@qq.com>
2016-06-15 08:32:34 +0800

    单纯的测试

--- app/src/main/java/cn/hsd/tutor/Stu_log/VistlogActivity.java
+++ app/src/main/java/cn/hsd/tutor/Stu_log/VistlogActivity.java
@@ -83,7 +83,7 @@ public class VistlogActivity extends AppCompatActivity {
                 conn.Postclient(path, infoResult, handler);
             }
 
-        }.start();
+        }.start();//单纯的测试
     }
 
 }
