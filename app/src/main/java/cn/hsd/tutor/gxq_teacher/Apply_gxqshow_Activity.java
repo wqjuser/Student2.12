d222e47c20897d8f1d27d54faea5df4c6ea7bd61
wenqingjie <316400644@qq.com>
2016-06-15 08:44:25 +0800

    添加了郭晓琴的代码，并且修正了代码

---
+++ app/src/main/java/cn/hsd/tutor/gxq_teacher/Apply_gxqshow_Activity.java
@@ -0,0 +1,337 @@
+package cn.hsd.tutor.gxq_teacher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import cn.hsd.student.R;
+import cn.hsd.student.activity.Json.json;
+import cn.hsd.student.activity.service.SDS_Httpclient;
+import cn.hsd.student.activity.service.SDS_ZMHandler;
+import cn.hsd.tutor.gxq_class.Signin_class;
+import cn.hsd.tutor.gxq_class.Gxq_apply_student;
+
+
+public class Apply_gxqshow_Activity extends AppCompatActivity {
+
+    /**
+     * 放置String类型的list    存储学生的姓名。
+     */
+    List<String> listgxq1 = new ArrayList<String>();
+
+ //   public List<Gxq_apply_student> shishiList1  = new ArrayList<Gxq_apply_student>();
+    public List<String> dataList1 = new ArrayList<String>();//返回的主要信息筛选过的
+    public List<Gxq_apply_student> personmessage  = new ArrayList<Gxq_apply_student>();
+    public List<Gxq_apply_student> personmessage2  = new ArrayList<Gxq_apply_student>();
+    public List<Gxq_apply_student> personmessage3  = new ArrayList<Gxq_apply_student>();
+    public List<Map> listmap =  new ArrayList<Map>();;
+
+    public Spinner applyshow;
+    public ListView applyshow_list;
+    private ArrayAdapter<String> adapter1;
+    public RadioGroup radioGroup;
+    public RadioButton radioButton1;
+    public RadioButton radioButton2;
+
+    private Gxq_apply_student lastshow_gxqapply;
+    public TextView starttime;
+    public TextView endtime;
+    public TextView apply_reason;
+    public Button click_result;
+    public Double result_id;
+    public String check_result;
+    private SDS_Httpclient conn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apply_gxqshow_);
+        applyshow = (Spinner) findViewById(R.id.spinner_studengapply);
+        applyshow_list = (ListView)findViewById(R.id.showapply_gxq) ;
+        radioGroup = (RadioGroup) findViewById(R.id.sex);
+        radioButton1 =(RadioButton) findViewById(R.id.male);
+        radioButton2 =(RadioButton) findViewById(R.id.female);
+        starttime = (TextView) findViewById(R.id.beginTime_gxq);
+        endtime = (TextView) findViewById(R.id.endTime_gxq);
+        apply_reason = (TextView) findViewById(R.id.apply_reason);
+        click_result = (Button) findViewById(R.id.click_result);
+        //-----------------------------------------------------------radioBUtton-----------------------------------------------------
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId==radioButton1.getId()){
+                    Toast.makeText(Apply_gxqshow_Activity.this, "选择了接收", Toast.LENGTH_SHORT).show();
+                    check_result = "同意";
+                }else if(checkedId==radioButton2.getId()){
+                    Toast.makeText(Apply_gxqshow_Activity.this, "选择了拒绝", Toast.LENGTH_SHORT).show();
+                    check_result = "拒绝";
+                }
+            }
+        });
+      //初始化listgxq1数据....................................................................................
+//        listgxq1.add("是非");
+//        listgxq1.add("李凤玉");
+//        listgxq1.add("自己");
+       //初始化List1数据
+//        Gxq_apply_student data = new Gxq_apply_student();
+//        data.setCounserlor_name("翟德斌");
+//        data.setStudent_name("是非");
+//        data.setLeave_begin("2016.5.1");
+//        data.setLeave_end("2016.5.2");
+//        data.setLeave_content("宝宝生病了");
+//        data.setLeave_check("未处理");
+//        data.setLeave_id(1);
+//        data.setCreate_time("今天");
+//
+//
+//        Gxq_apply_student data1 = new Gxq_apply_student();
+//        data1.setCounserlor_name("检讨");
+//        data1.setStudent_name("李凤玉");
+//        data1.setLeave_begin("2016.8.1");
+//        data1.setLeave_end("2016.7.2");
+//        data1.setLeave_content("宝宝的姐姐生病了");
+//        data1.setLeave_check("未处理");
+//        data1.setLeave_id(2);
+//        data1.setCreate_time("明天");
+//
+//        Gxq_apply_student data3 = new Gxq_apply_student();
+//        data3.setCounserlor_name("翟德斌");
+//        data3.setStudent_name("李凤玉");
+//        data3.setLeave_begin("2016.6.1");
+//        data3.setLeave_end("2016.7.2");
+//        data3.setLeave_content("呵呵打");
+//        data3.setLeave_check("未处理");
+//        data3.setLeave_id(3);
+//        data3.setCreate_time("后天");
+//
+//        Gxq_apply_student data2 = new Gxq_apply_student();
+//        data2.setCounserlor_name("是非");
+//        data2.setStudent_name("自己");
+//        data2.setLeave_begin("2016.5.1.1");
+//        data2.setLeave_end("2016.5.2");
+//        data2.setLeave_content("宝宝的宝宝的宝宝生病了");
+//        data2.setLeave_check("未处理");
+//        data2.setLeave_id(4);
+//        data2.setCreate_time("大后天");
+//
+//        shishiList1.add(data);
+//        shishiList1.add(data1);
+//        shishiList1.add(data2);
+//        shishiList1.add(data2);
+//        shishiList1.add(data3);
+//        shishiList1.add(data3);
+        //..........................................................................................................................
+//        ArrayAdapter<String> apply_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listgxq1);
+//        applyshow.setAdapter(apply_adapter);
+//        applyshow.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//            //    Toast.makeText(Apply_gxqshow_Activity.this, "你点击的是:"+listgxq1.get(position), Toast.LENGTH_SHORT).show();
+//                //spinner = apply_spinner[position];
+//                String aa = listgxq1.get(position);
+//                dataList1 = diaoyong(shishiList1,aa);
+//                adapter1.notifyDataSetChanged();
+//            }
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+        //--------------------------------------------------------------------------------------------------------------------------------..
+        adapter1 = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, dataList1);
+        applyshow_list.setAdapter(adapter1);
+        applyshow_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                lastshow_gxqapply =  personmessage.get(position);
+                starttime.setText(lastshow_gxqapply.getLeave_begin());
+                endtime.setText(lastshow_gxqapply.getLeave_end());
+                apply_reason.setText(lastshow_gxqapply.getLeave_content());
+                result_id = lastshow_gxqapply.getLeave_id();
+            }
+        });
+        //----------------------------------------------------------------------------------------------------------------------------------
+
+
+    }
+
+    public List<String> diaoyong(List<Gxq_apply_student>  backdata ,String name) {
+
+        if (backdata.size() > 0) {
+            dataList1.clear();
+            personmessage.clear();
+            for (Gxq_apply_student provience : backdata) {
+                //    Toast.makeText(Gxq_apply_main.this, provience.getApply_student_beginname(), Toast.LENGTH_SHORT).show();
+                if((provience.getStudent_name()).equals(name)){
+                    dataList1.add(provience.getCreate_time());
+                    personmessage.add(provience);
+                }
+            }
+        }
+        return dataList1;
+    }
+  public void click(View view){
+      switch (view.getId()){
+      case R.id.click_result:
+             if ((starttime.getText()).equals("开始时间")){
+                 Toast.makeText(Apply_gxqshow_Activity.this, "请选择处理的假条", Toast.LENGTH_SHORT).show();
+             }else {
+             connect_tijiao();
+             }
+
+      break;
+      default:
+          break;
+      }
+  }
+    //-------------------------------------提交教师返回的假条 处理数据-------------------------------------------------------------------------
+    public SDS_ZMHandler gxq_teacherbackhandler = new SDS_ZMHandler(){
+        @Override
+        public void onSuccess(String content) {
+            super.onSuccess(content);
+            Toast.makeText(Apply_gxqshow_Activity.this, "与服务器连接成功", Toast.LENGTH_SHORT).show();
+            json<String> gxqreturn = new json<String>();
+        }
+
+        @Override
+        public void onFailture(String content) {
+            super.onFailture(content);
+            Toast.makeText(Apply_gxqshow_Activity.this, "失败", Toast.LENGTH_SHORT).show();
+        }
+    };
+    public void connect_tijiao(){
+        new Thread(){
+            public void run(){
+                String path = "http://192.168.191.1:8080/MyHsd/submitxsxs";
+                conn = new SDS_Httpclient();
+               Gxq_apply_student data = new Gxq_apply_student();
+                data.setLeave_id(result_id);
+                data.setLeave_check(check_result);
+                json<Signin_class> dataVerture = new json<Signin_class>();
+                String returnJson = dataVerture.ObjectToJson1(data);
+                conn.Postclient(path,returnJson,gxq_teacherbackhandler);
+            }
+        }.start();
+    }
+//----------------------------------------------------获取学生姓名------------------------------------------------------------
+public void connect_studentname(){
+    new Thread(){
+        public void run(){
+            String path = "http://192.168.191.1:8080/MyHsd/xsxs";
+            conn = new SDS_Httpclient();
+//            Signin_class data = new Signin_class();
+//            json<Signin_class> dataVerture = new json<Signin_class>();
+//            String returnJson = dataVerture.ObjectToJson1(data);
+////            String returnJson ="";
+            conn.Postclient(path,"",gxq_studentnaehandler);
+        }
+    }.start();
+}
+    public SDS_ZMHandler gxq_studentnaehandler = new SDS_ZMHandler(){
+        @Override
+        public void onSuccess(String content) {
+            super.onSuccess(content);
+            //          Toast.makeText(gxq_teacher_Activity.this, "与服务器连接成功", Toast.LENGTH_SHORT).show();
+            json<String> gxqreturn = new json<String>();
+
+            listgxq1= gxqreturn.jsonToList2(content);
+            ArrayAdapter<String> apply_adapter = new ArrayAdapter<String>(Apply_gxqshow_Activity.this, android.R.layout.simple_spinner_item, listgxq1);
+            applyshow.setAdapter(apply_adapter);
+
+
+        }
+        @Override
+        public void onFailture(String content) {
+            super.onFailture(content);
+            Toast.makeText(Apply_gxqshow_Activity.this, "失败", Toast.LENGTH_SHORT).show();
+        }
+    };
+    //---------------------------------------------------------------------------------------------------------------------------------------
+    //------------------------------------获取信息List（Gxq_apply——student）--------------------------------------------------------------
+    public void connect_apply(){
+        new Thread(){
+            public void run(){
+                String path = "http://192.168.191.1:8080/MyHsd/apply";
+                conn.Postclient(path,"",gxq_apply_handler);
+            }
+        }.start();
+    }
+    public SDS_ZMHandler gxq_apply_handler = new SDS_ZMHandler(){
+        @Override
+        public void onSuccess(String content) {
+            super.onSuccess(content);
+        //    Toast.makeText(Apply_gxqshow_Activity.this, "与服务器连接成功", Toast.LENGTH_SHORT).show();
+            personmessage2.clear();
+            listmap.clear();
+            personmessage3.clear();
+            json<Gxq_apply_student> gxqreturn1 = new json<Gxq_apply_student>();
+            personmessage2= gxqreturn1.jsonToList2(content);
+            for(int i=0;i<personmessage2.size();i++){
+                Map map = (Map) personmessage2.get(i);
+                listmap.add(map);
+            }
+            for (Map ll : listmap){
+                Gxq_apply_student rr = new Gxq_apply_student();
+                Double qq = (Double) ll.get("leave_id");
+                String ww = (String) ll.get("create_time");
+                String ee = (String) ll.get("student_name") ;
+                String tt = (String) ll.get("counserlor_name") ;
+                String yy = (String) ll.get("leave_content") ;
+                String uu = (String) ll.get("leave_begin") ;
+                String ii = (String) ll.get("leave_end") ;
+                String oo = (String) ll.get("leave_check") ;
+                 rr.setLeave_id(qq);
+                rr.setCreate_time(ww);
+                rr.setStudent_name(ee);
+                rr.setCounserlor_name(tt);
+                rr.setLeave_content(yy);
+                rr.setLeave_begin(uu);
+                rr.setLeave_end(ii);
+                rr.setLeave_check(oo);
+
+                personmessage3.add(rr);
+            }
+
+            applyshow.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    //    Toast.makeText(Apply_gxqshow_Activity.this, "你点击的是:"+listgxq1.get(position), Toast.LENGTH_SHORT).show();
+                    //spinner = apply_spinner[position];
+                    String aa = listgxq1.get(position);
+                    dataList1 = diaoyong(personmessage3,aa);
+                    adapter1.notifyDataSetChanged();
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
+
+        @Override
+        public void onFailture(String content) {
+            super.onFailture(content);
+            Toast.makeText(Apply_gxqshow_Activity.this, "失败", Toast.LENGTH_SHORT).show();
+        }
+    };
+    //---------------------------------------------------------------------------------------------------------------------------------------
+    @Override
+    protected void onResume() {
+        super.onResume();
+        connect_studentname();
+        connect_apply();
+    }
+}
---
+++ app/src/main/java/cn/hsd/tutor/gxq_teacher/gxq_teacher_Activity.java
@@ -0,0 +1,207 @@
+package cn.hsd.tutor.gxq_teacher;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import cn.hsd.student.R;
+import cn.hsd.student.activity.Json.json;
+import cn.hsd.student.activity.service.SDS_Httpclient;
+import cn.hsd.student.activity.service.SDS_ZMHandler;
+import cn.hsd.tutor.gxq_class.Signin_class;
+
+
+public class gxq_teacher_Activity extends AppCompatActivity {
+    public Spinner spinner_teacher;
+    public TextView qiandaoshow_gxq;
+
+    public List<Signin_class> backList;
+    public List<Map> list;
+    private SDS_Httpclient conn;
+    List<String> listgxq = new ArrayList<String>();
+    List<String> listgxq22222222 = new ArrayList<String>();
+    public List<Signin_class> shishiList  = new ArrayList<Signin_class>();
+    public List<Signin_class> shishiList2  = new ArrayList<Signin_class>();
+    public List<Signin_class> shishiList3  = new ArrayList<Signin_class>();
+    ArrayAdapter<String> apply_message_adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gxq_teacher_);
+      //  connectserv();
+        list = new ArrayList<Map>();
+        spinner_teacher = (Spinner) findViewById(R.id.spinner_teacher);
+        qiandaoshow_gxq = (TextView) findViewById(R.id.qidaoshow_gxq);
+        backList = new ArrayList<Signin_class>();
+//
+//        Signin_class data = new Signin_class();
+//        data.setStudent_name("aaa");
+//        data.setSign_address("哈尔滨师范大学");
+//        data.setSign_time("2016.10.55");
+//
+//        Signin_class data1 = new Signin_class();
+//        data1.setStudent_name("bbb");
+//        data1.setSign_address("北京师范大学");
+//        data1.setSign_time("2016.18.55");
+//
+//        Signin_class data2 = new Signin_class();
+//        data2.setStudent_name("ccc");
+//        data2.setSign_address("长春师范大学");
+//        data2.setSign_time("2016.08.55");
+//
+//        shishiList.add(data);
+//        shishiList.add(data1);
+//        shishiList.add(data2);
+//        shishiList.add(data2);
+//
+//  //      listgxq.add("请刷新数据");
+
+//         spinner_teacher.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                String aa = listgxq.get(position);
+//                String bb;
+//                bb =   diaoyong(shishiList,aa);
+//                qiandaoshow_gxq.setText(bb);
+//            }
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+
+    }
+
+    public String  diaoyong(List<Signin_class>  shidata,String name) {
+        String qiandao = "";
+        if (shidata.size() > 0) {
+            for (Signin_class provience : shidata) {
+
+                if ((provience.getStudent_name()).equals(name)) {
+                     qiandao = qiandao+ provience.getSign_time() +provience.getSign_address()+"\n";
+
+                }
+            }
+        }
+        return qiandao;
+    }
+
+    public SDS_ZMHandler gxq_zmhandler = new SDS_ZMHandler(){
+        @Override
+        public void onSuccess(String content) {
+            super.onSuccess(content);
+  //          Toast.makeText(gxq_teacher_Activity.this, "与服务器连接成功", Toast.LENGTH_SHORT).show();
+           json<String> gxqreturn = new json<String>();
+
+           listgxq= gxqreturn.jsonToList2(content);
+            apply_message_adapter = new ArrayAdapter<String>(gxq_teacher_Activity.this, android.R.layout.simple_spinner_item, listgxq);
+            spinner_teacher.setAdapter(apply_message_adapter);
+
+
+        }
+        @Override
+        public void onFailture(String content) {
+            super.onFailture(content);
+            Toast.makeText(gxq_teacher_Activity.this, "失败", Toast.LENGTH_SHORT).show();
+        }
+    };
+public void connectserv(){
+    new Thread(){
+        public void run(){
+            String path = "http://192.168.191.1:8080/MyHsd/xsxs";
+            conn = new SDS_Httpclient();
+//            Signin_class data = new Signin_class();
+//            json<Signin_class> dataVerture = new json<Signin_class>();
+//            String returnJson = dataVerture.ObjectToJson1(data);
+//            String returnJson ="";
+             conn.Postclient(path,"",gxq_zmhandler);
+        }
+    }.start();
+}
+    public void connect(){
+        new Thread(){
+            public void run(){
+                String path = "http://192.168.191.1:8080/MyHsd/gxqxsxs";
+                conn = new SDS_Httpclient();
+                Signin_class data = new Signin_class();
+                json<Signin_class> dataVerture = new json<Signin_class>();
+                String returnJson = dataVerture.ObjectToJson1(data);
+//            String returnJson ="";
+                conn.Postclient(path,returnJson,gxq_conhandler);
+            }
+        }.start();
+    }
+    public SDS_ZMHandler gxq_conhandler = new SDS_ZMHandler(){
+        @Override
+        public void onSuccess(String content) {
+            super.onSuccess(content);
+            Toast.makeText(gxq_teacher_Activity.this, "与服务器连接成功", Toast.LENGTH_SHORT).show();
+            shishiList2.clear();
+            shishiList3.clear();
+            list.clear();
+           json<Signin_class> gxqreturn1 = new json<Signin_class>();
+            shishiList2= gxqreturn1.jsonToList2(content);
+            for(int i=0;i<shishiList2.size();i++){
+                Map map = (Map) shishiList2.get(i);
+                list.add(map);
+            }
+            for (Map ll : list){
+                Signin_class rr = new Signin_class();
+                String qq = (String) ll.get("sign_address");
+                String ww = (String) ll.get("student_name");
+                String ee = (String) ll.get("sign_time") ;
+                rr.setStudent_name(ww);
+                rr.setSign_address(qq);
+                rr.setSign_time(ee);
+                shishiList3.add(rr);
+            }
+
+            spinner_teacher.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    String aa = listgxq.get(position);
+                    String bb;
+                    bb =   diaoyong(shishiList3,aa);
+                    qiandaoshow_gxq.setText(bb);
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
+
+        @Override
+        public void onFailture(String content) {
+            super.onFailture(content);
+            Toast.makeText(gxq_teacher_Activity.this, "失败", Toast.LENGTH_SHORT).show();
+        }
+    };
+    public void click(View view){
+        switch (view.getId()){
+            case R.id.shuxin:
+                Intent intent1 = new Intent(gxq_teacher_Activity.this,Apply_gxqshow_Activity.class);
+                startActivity(intent1);
+                break;
+            default:
+                break;
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        connectserv();
+        connect();
+    }
+}
